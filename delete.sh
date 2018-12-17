f3df4f80a4d5cd3903de6515a5d9bbee15564785
hettling <hannes.hettling@gmail.com>
2018-12-17 17:14:55 +0100

    debug

--- delete.sh
+++ delete.sh
@@ -10,7 +10,7 @@ for DIR in "${DIRS[@]}"
 do
 	COLTYPE="testset-$DIR"
 	echo "Deleting data for collection type $COLTYPE from $DIR"			
-	
-	curl -XPOST $SERVER/$INDEX/$DOCTYPE/_delete_by_query -d \'"{ \"query\": {\"term\": {\"collectionType\" : \"$COLTYPE\"}} }"\'
+	   
+	curl -XPOST $SERVER/$INDEX/$DOCTYPE/_delete_by_query -d \'{ \"query\": {\"term\": {\"collectionType\" : \"$COLTYPE\"}} }\'
 done
 
