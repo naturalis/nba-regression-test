13b7da184145e216f3bead2b2d4346ed2f01e8fa
hettling <hannes.hettling@gmail.com>
2018-12-17 14:40:50 +0100

    adding exec permissions

--- insert.sh
+++ insert.sh
@@ -8,9 +8,7 @@ declare -a DIRS=(specimen-query specimen-query-geo specimen-groupByScientificNam
 
 for DIR in "${DIRS[@]}"
 do		
-
-	FILE="$DIR/data.json"
-	
+	FILE="$DIR/data.json"	
 	echo "Importing data from $FILE"
 	while read ENTRY; do
 		curl -w "\n" -XPOST $SERVER/$INDEX/$DOCTYPE -d "$ENTRY"
