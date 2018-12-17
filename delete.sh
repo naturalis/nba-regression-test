9f7f2b02b0ffc6f0debd228b02a22524203e24f7
hettling <hannes.hettling@gmail.com>
2018-12-17 17:20:39 +0100

    next try

--- delete.sh
+++ delete.sh
@@ -9,8 +9,9 @@ declare -a DIRS=(specimen-query specimen-query-geo specimen-groupByScientificNam
 for DIR in "${DIRS[@]}"
 do
 	COLTYPE="testset-$DIR"
+	echo
 	echo "Deleting data for collection type $COLTYPE from $DIR"			
 	   
-	curl -XPOST $SERVER/$INDEX/$DOCTYPE/_delete_by_query -d \'{ \"query\": {\"term\": {\"collectionType\" : \"$COLTYPE\"}} }\'
+	curl -XPOST $SERVER/$INDEX/$DOCTYPE/_delete_by_query -d '{ "query": {"term": {"collectionType" : "'$COLTYPE'"}} }'
 done
 
