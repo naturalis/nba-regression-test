d6a56586a609b58816cd3664c2dfef5265b8465b
hettling <hannes.hettling@gmail.com>
2018-12-17 14:37:51 +0100

    adding insert and delete scripts

---
+++ delete.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SERVER="http://localhost:9200"
+INDEX="specimen"
+DOCTYPE="Specimen"
+
+declare -a DIRS=(specimen-query specimen-query-geo specimen-groupByScientificName)
+
+for DIR in "${DIRS[@]}"
+do
+	COLTYPE="testset-$DIR"
+	echo "Deleting data for collection type $COLTYPE from $DIR"			
+	
+	curl -XPOST $SERVER/$INDEX/$DOCTYPE/_delete_by_query -d '{ "query": {"term": {"collectionType" : "$COLTYPE"}} }'
+done
+
---
+++ insert.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SERVER="http://localhost:9200"
+INDEX="specimen"
+DOCTYPE="Specimen"
+
+declare -a DIRS=(specimen-query specimen-query-geo specimen-groupByScientificName)
+
+for DIR in "${DIRS[@]}"
+do		
+
+	FILE="$DIR/data.json"
+	
+	echo "Importing data from $FILE"
+	while read ENTRY; do
+		curl -w "\n" -XPOST $SERVER/$INDEX/$DOCTYPE -d "$ENTRY"
+	done < $FILE
+	
+done
+
