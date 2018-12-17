8ba1475162bdc4a565d0946b29222539acd9b101
hettling <hannes.hettling@gmail.com>
2018-12-17 15:07:32 +0100

    debug out

--- insert.sh
+++ insert.sh
@@ -11,7 +11,7 @@ do
 	FILE="$DIR/data.json"	
 	echo "Importing data from $FILE"
 	while read ENTRY; do
-		echo curl -w "\n" -XPOST $SERVER/$INDEX/$DOCTYPE -d "$ENTRY"
+		curl -w "\n" -XPOST $SERVER/$INDEX/$DOCTYPE -d "$ENTRY"
 	done < $FILE
 	
 done
